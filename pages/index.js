d76218a9b95820c366a39b493b10ba14898a8b1f
Adrian Paiva <114938678+opaiiva@users.noreply.github.com>
2026-01-20 22:44:34 +0000

    adiciona o status de `Em construcao`

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 function Home() {
   return (
-    <h1>
-      Estamos neste exato momento construindo um novo local na internet para
-      quem <b>trabalha com tecnologia</b> e precisa consumir conteúdos com{" "}
-      <b>valor concreto</b>. Somos pessoas brutalmente exatas e empáticas,{" "}
-      <b>simultaneamente</b>, onde o termômetro para entender se isso está sendo
-      aplicado é simples: as pessoas estão se <b>afastando</b> ou se{" "}
-      <b>aproximando?</b>{" "}
-    </h1>
+    <div>
+      <h1>Em Construção...</h1>
+      <h3>
+        Estamos neste exato momento construindo um novo local na internet para
+        quem <b>trabalha com tecnologia</b> e precisa consumir conteúdos com{" "}
+        <b>valor concreto</b>. Somos pessoas brutalmente exatas e empáticas,{" "}
+        <b>simultaneamente</b>, onde o termômetro para entender se isso está
+        sendo aplicado é simples: as pessoas estão se <b>afastando</b> ou se{" "}
+        <b>aproximando?</b>{" "}
+      </h3>
+    </div>
   );
 }
 
