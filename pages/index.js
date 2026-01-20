81c091c30efe8450fbcf405f5f767cbeb57b06da
Adrian Paiva <114938678+opaiiva@users.noreply.github.com>
2026-01-20 22:44:34 +0000

    adiciona o status de

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
-function Home() {
+function Title() {
+  return <h1>Em Construção...</h1>;
+}
+
+function Paragraph() {
   return (
-    <h1>
+    <h3>
       Estamos neste exato momento construindo um novo local na internet para
       quem <b>trabalha com tecnologia</b> e precisa consumir conteúdos com{" "}
       <b>valor concreto</b>. Somos pessoas brutalmente exatas e empáticas,{" "}
       <b>simultaneamente</b>, onde o termômetro para entender se isso está sendo
       aplicado é simples: as pessoas estão se <b>afastando</b> ou se{" "}
       <b>aproximando?</b>{" "}
-    </h1>
+    </h3>
   );
 }
 
-export default Home;
+export default Title & Paragraph;
